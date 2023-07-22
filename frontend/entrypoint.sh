fb281a4c7170e3ed36030dfb723ec9e558270e35
MuNeNICK <72444129+MuNeNICK@users.noreply.github.com>
2023-07-23 02:11:09 +0900

    Update entrypoint.sh

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,10 +2,6 @@
 
 cd /usr/src/app
 
-if [ ! -d "node_modules" ]; then
-    yarn create next-app . --typescript --eslint
-fi
-
 while read line
 do
   if yarn list --pattern "$line" > /dev/null; then
