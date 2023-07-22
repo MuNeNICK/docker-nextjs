e47c2a352c3ef7772635abf18c1d1acccfc6a2dd
MuNeNICK <72444129+MuNeNICK@users.noreply.github.com>
2023-07-23 02:18:46 +0900

    Update entrypoint.sh

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,15 +2,17 @@
 
 cd /usr/src/app
 
-while read line
-do
-  if yarn list --pattern "$line" > /dev/null; then
-    echo $line is EXIST.
-  else
-    echo $line is NOT EXIST.
-    yarn add $line
-  fi
-done < /packages.txt
+check_and_install_packages() {
+  while read line
+  do
+    if yarn list --pattern "$line" > /dev/null; then
+      echo $line is EXIST.
+    else
+      echo $line is NOT EXIST.
+      yarn add $line
+    fi
+  done < /packages.txt
+}
 
 case "$STATUS" in
   "installation")
@@ -20,10 +22,12 @@ case "$STATUS" in
     ;;
   "development")
     echo Starting in developer mode...
+    check_and_install_packages
     yarn dev
     ;;
   "production")
     echo Starting in production mode...
+    check_and_install_packages
     yarn build
     yarn start
     ;;
