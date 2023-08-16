0498342bb71db8bdcb59ff7cfbcc8e783857b749
MuNeNICK <72444129+MuNeNICK@users.noreply.github.com>
2023-08-16 16:12:27 +0900

    Update entrypoint.sh

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,32 +2,50 @@
 
 cd /usr/src/app
 
-check_and_install_packages() {
+install_packages() {
   while read line
   do
-    if yarn list --pattern "$line" > /dev/null; then
-      echo $line is EXIST.
+    result=$(yarn list --pattern "$line")
+
+    if echo "$result" | grep -q "$line"; then
+      echo "$line" is EXIST.
     else
-      echo $line is NOT EXIST.
-      yarn add $line
+      echo "$line" is NOT EXIST.
+      yarn add "$line"
     fi
   done < /packages.txt
+}
+
+uninstall_removed_packages() {
+  if [ -f .current_packages.txt ]; then
+    while read -r package; do
+      if ! grep -q "^$package$" /packages.txt; then
+        echo "$package" is NOT in packages.txt.
+        yarn remove "$package"
+      fi
+    done < .current_packages.txt
+  fi
 }
 
 case "$STATUS" in
   "installation")
     echo Starting in installation mode...
-    echo RUN yarn create next-app .
+    echo Please execute the command below
+    echo docker compose exec frontend yarn create next-app .
     bash
     ;;
   "development")
     echo Starting in developer mode...
-    check_and_install_packages
+    install_packages
+    uninstall_removed_packages
+    cp /packages.txt /usr/src/app/.current_packages.txt
     yarn dev
     ;;
   "production")
     echo Starting in production mode...
-    check_and_install_packages
+    install_packages
+    uninstall_removed_packages
+    cp /packages.txt /usr/src/app/.current_packages.txt
     yarn build
     yarn start
     ;;
